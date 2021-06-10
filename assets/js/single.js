7ada49eccb9cce08fc9dce12b53b12443455fd05
Owen <ochilongo@gmail.com>
2021-06-10 11:16:54 -0500

    Added Extra Message

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
   console.log(repo);
@@ -12,6 +13,11 @@ var getRepoIssues = function (repo) {
       response.json().then(function (data) {
         // pass response data to dom function
         displayIssues(data);
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+            displayWarning(repo);
+        }
       });
     } else {
       alert("There was a problem with your request!");
@@ -55,6 +61,21 @@ var displayIssues = function (issues) {
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
+};
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    // create linke element
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+
 };
 
 getRepoIssues("facebook/react");
